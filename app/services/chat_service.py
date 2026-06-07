fcf68a182af710a5baf391ecafcc2880e290f0d3
unknown <mohammadhg98@gmail.com>
2026-06-07 21:55:12 +0330

    langchain import update

--- app/services/chat_service.py
+++ app/services/chat_service.py
@@ -2,7 +2,7 @@
 from typing import Dict, List, Optional, Any
 import json
 import re
-from langchain_community.chat_models import ChatOpenAI
+from langchain_openai import ChatOpenAI
 import logging
 from loguru import logger
 
